27681783be94bad6d77f143d8865011c314f3ca7
000Cube <88157934+000Cube@users.noreply.github.com>
2021-09-14 23:27:32 +0900

    クッキーの送信

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,26 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use App\Http\Requests\CookieRequest;
 use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request){
-        //Validatorクラスの利用
-        /* $validator = Validator::make($request->query(),[
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if($validator->fails()){
-            $msg = 'クエリーに問題があります。';
-        } else{
-            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
-        } */
-        return view('hello.index',['msg' => 'フォームを入力してください。']);
+        if($request->hasCookie('msg')){
+            $msg = 'COOKIE:'.$request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index',['msg' => $msg]);
     }
 
-    public function post(HelloRequest $request){
-        return view('hello.index',['msg' => '正しく入力されました！']);
+    public function post(CookieRequest $request){
+        $msg = $request->msg;
+        $response = new Response(view('hello.index',[
+            'msg' => "『{$msg}』をクッキーに保存しました。"
+        ]));
+        $response->cookie('msg',$msg,100);
+        return $response;
     }
 
     public function other(){
